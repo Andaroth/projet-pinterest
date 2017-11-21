3496902c0cc14b1ec4b2b192b2b8fdd218c5ba2e
Anda <ax.fiolle@gmail.com>
2017-11-21 14:42:28 +0100

    update CSS

--- Views/all.php
+++ Views/all.php
@@ -2,7 +2,8 @@
 <html lang="en"> 
 <head> 
   <meta charset="UTF-8"> 
-  <title><?= "hello world" ?></title> 
+  <title><?= "hello world" ?></title>
+  <link href="https://fonts.googleapis.com/css?family=Nunito%7COpen+Sans" rel="stylesheet">
   <link rel="stylesheet" href="./Views/css/style.css" />
 </head> 
 <body> 
