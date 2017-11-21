f82c19264e66306e0fe09dafe9ec5961aebdb11f
Anda <ax.fiolle@gmail.com>
2017-11-21 15:14:59 +0100

    patch

--- Views/js/app.js
+++ Views/js/app.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+  
   var elem = document.querySelector('.grid');
   var msnry = new Masonry( elem, {
     // options
     itemSelector: '.grid-item',
-    columnWidth: 200
+    columnWidth: function() {
+      return (elem.width /4);// depends how many boxes per row
+    }()
   });
 
   // element argument can be a selector string
