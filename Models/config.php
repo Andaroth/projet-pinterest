ff509edcddde7714fbba2771fcf6bf44ce349865
Anda <ax.fiolle@gmail.com>
2017-11-22 10:24:23 +0100

    test

--- Models/config.php
+++ Models/config.php
@@ -1,6 +1,6 @@
 <?php
   // auth to db
-  $dbhost = "localhost"; //chemin vers le serveur
+  $dbhost = "vps365316.ovh.net"; //chemin vers le serveur
   $dbname = "projet_pinterest"; //nom de la base de données
   $dbuser = "root_pinterest"; // nom utilisateur pour se connecter
   $dbpass = "pinterest"; // mot de passe pour se connecter
