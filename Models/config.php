5dd8fe99ed6c39c159312e940d880f27c562d609
Anda <ax.fiolle@gmail.com>
2017-11-24 09:32:56 +0100

    Connected to Heroku

--- Models/config.php
+++ Models/config.php
@@ -1,13 +1,13 @@
 <?php
   // auth to db
-  $dbhost = "localhost"; //chemin vers le serveur
-  $dbname = "projet_pinterest"; //nom de la base de données
-  $dbuser = "root_pinterest"; // nom utilisateur pour se connecter
-  $dbpass = "pinterest"; // mot de passe pour se connecter
+  $dbhost = "ec2-46-137-174-67.eu-west-1.compute.amazonaws.com"; //chemin vers le serveur
+  $dbname = "d14ccnf0ha0ble"; //nom de la base de données
+  $dbuser = "ynwyizvjfafzwu"; // nom utilisateur pour se connecter
+  $dbpass = "b47e04aac67ae3fd247fd2642580ce00c42a7586de1637dd9a7b048a1099b2b5"; // mot de passe pour se connecter
 
   // Connexion à la base de données.
 	try {
-		$bdd = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';charset=utf8',$dbuser,$dbpass);
+		$bdd = new PDO('pgsql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpass);
 	}
 	catch(Exception $e) {
 	  die('Erreur PDO : '.$e->getMessage());
