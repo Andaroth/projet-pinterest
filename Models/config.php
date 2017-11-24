028cfeb939307e2e7a394e6eb90f790454c56c83
Unknown <n.vanroost@gmail.com>
2017-11-24 13:44:48 +0100

    commit du "j'aime pas les conflicts" (stop change that)

--- Models/config.php
+++ Models/config.php
@@ -1,22 +1,19 @@
 <?php
   // auth to db
-  // $dbhost = "localhost"; //chemin vers le serveur
-  // $dbname = "projet_pinterest"; //nom de la base de données
-  // $dbuser = "user"; // nom utilisateur pour se connecter
-  // $dbpass = "user"; // mot de passe pour se connecter
+  //$dbhost = "localhost"; //chemin vers le serveur
+  //$dbname = "projet_pinterest"; //nom de la base de données
+  //$dbuser = "root_pinterest"; // nom utilisateur pour se connecter
+  //$dbpass = "pinterest"; // mot de passe pour se connecter
   $dbhost = "ec2-46-137-174-67.eu-west-1.compute.amazonaws.com"; //chemin vers le serveur
   $dbname = "d14ccnf0ha0ble"; //nom de la base de données
   $dbuser = "ynwyizvjfafzwu"; // nom utilisateur pour se connecter
-  $dbpass = "b47e04aac67ae3fd247fd2642580ce00c42a7586de1637dd9a7b048a1099b2b5"; // mot de passe pour se connecter
-
+  $dbpass = "b47e04aac67ae3fd247fd2642580ce00c42a7586de1637dd9a7b048a1099b2b5"; // mot de passe pour se connecter*/
   // Connexion à la base de données.
 	try {
-		$bdd = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';charset=utf8',$dbuser,$dbpass);
-    //$bdd = new PDO('pgsql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpass);
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		//$bdd = new PDO('mysql:host='.$dbhost.';dbname='.$dbname.';charset=utf8',$dbuser,$dbpass);
+    $bdd = new PDO('pgsql:host='.$dbhost.';dbname='.$dbname,$dbuser,$dbpass);
 	}
 	catch(Exception $e) {
 	  die('Erreur PDO : '.$e->getMessage());
 	}
-
 ?>
