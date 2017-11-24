a17f5a43d281347f6d4394435e57997dbe7e313e
debraive <32301367+debraive@users.noreply.github.com>
2017-11-24 14:15:49 +0100

    css

--- Views/modals.php
+++ Views/modals.php
@@ -1,28 +1,20 @@
 <div class="modal">
     <div class="modal-content">
-    <span class="close">&times;</span>
-    <div class="logo">
-      <img src="Views/img/Pinto.svg" alt="logo">
-    </div>
-    <div class="signin">
-      <h3>New on pinTO? sign up </h3>
-      <div class="form1">
-        <?php include V."register.php"; ?>
+      <span class="close">&times;</span>
+      <div class="logo">
+        <img src="Views/img/Pinto.svg" alt="logo">
+      </div>
+      <div class="signin">
+        <h3>New on pinTO? sign up </h3>
+        <div class="form1">
+          <?php include V."register.php"; ?>
+        </div>
+      </div>
+      <div class="signup">
+        <h3>Have an account? log in</h3>
+        <div class="form2">
+          <?php include V."login.php"; ?>
+        </div>
       </div>
     </div>
-    <div class="signup">
-      <h3>Have an account? log in</h3>
-      <div class="form2">
-        <?php include V."login.php"; ?>
-      </div>
-
-
-    </div>
-
-    </div>
   </div>
-  </body>
-</html>
-
-  </div>
-</div>
