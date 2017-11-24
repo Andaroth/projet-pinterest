6e50938a0abde461daf16c4fd88eff7f8e54e58a
weichuan.tran <weichuan.tran@gmail.com>
2017-11-24 15:11:40 +0100

    class account ok

--- Models/config.php
+++ Models/config.php
@@ -18,4 +18,8 @@
 	  die('Erreur PDO : '.$e->getMessage());
 	}
 
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+
 ?>
--- Views/register.php
+++ Views/register.php
@@ -1,4 +1,4 @@
-<form action="./" method="post">
+<form action="./?action=register" method="post">
   Nom d'utilisateur : <input type="text" name="name" value=""></br>
   Addresse mail : <input type="text" name="mail" value=""></br>
   Mot de passe : <input type="password" name="pass" value=""></br>
