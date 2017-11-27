0b1d89f4a6298aab6a3e85204c762aa6e6f4a03a
weichuan.tran <weichuan.tran@gmail.com>
2017-11-27 09:38:17 +0100

    update

--- Controllers/class.php
+++ Controllers/class.php
@@ -8,11 +8,13 @@ class Account {
 		// extract($_GET);
 		switch($_GET["action"]) {
 			case 'register':
-				$name = $_POST['name'];
-				$mail = $_POST['mail'];
-				$pass = $_POST['pass'];
-				$pass = hash("sha256", htmlentities($_POST['pass']) ); // hash le password
-				$this->Signup($name,$mail,$pass);
+				if () {
+					$name = $_POST['name'];
+					$mail = $_POST['mail'];
+					$pass = $_POST['pass'];
+					$pass = hash("sha256", htmlentities($_POST['pass']) ); // hash le password
+					$this->Signup($name,$mail,$pass);
+				}
 				break;
 			case 'login':
 				$name = $_POST["username"];
