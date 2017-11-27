eda20fa7e0b8218575cbb98d71ac4fb7c81d2f4d
Anda <ax.fiolle@gmail.com>
2017-11-27 15:55:53 +0100

    WIP

--- Views/onepicture.php
+++ Views/onepicture.php
@@ -1,5 +1,5 @@
   <div id="onepic" class="modal modalphoto hidden">
-    <a id="calltoclose" class="close" href="#">&times;</a>
+    <a id="calltoclose" class="close closeimg" href="#">&times;</a>
     <img id="imageview" src="<?= imghold ?>" alt="oneView">
     <h2 id="imagetitle"></h2>
     <p id="imagedescr"></p>
