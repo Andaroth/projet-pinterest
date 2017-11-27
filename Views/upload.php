f875fe87e5f323c446148a69068aecd51681a9ec
Delphine <delphine@MacBook-Pro-de-Delphine.local>
2017-11-27 16:54:48 +0100

    form

--- Views/upload.php
+++ Views/upload.php
@@ -1,34 +1,39 @@
 <div class="upload_img">
   <form action="upload.php" method="post" enctype="multipart/form-data">
-    Ajoutez une photo à votre compte : <br>
+    <div class="new_img">
+      Une nouvelle image?  <br>
     <input type="file" name="fileToUpload" id="fileToUpload"> <br>
     <div class="img_upload">
     </div>
+    </div>
     <div class="description">
-
+    <div class="submit">
     <input type="submit" value="Upload Image" name="submit"><br>
-    Titre:
+    </div>
+    Titre <br>
     <input type="text" name="title"><br>
-    Description: <br>
+    Description<br>
     <textarea name="description" id="description" cols="30" rows="10"></textarea><br>
-   <p>
+      <p>
        Cochez la/les catégories souhaitées:<br />
        <input type="checkbox" name="nature" id="nature" /> <label for="nature">Nature</label>
        <input type="checkbox" name="voyage" id="voyage" /> <label for="voyage">Voyages</label>
        <input type="checkbox" name="dessin" id="dessin" /> <label for="dessin">Dessin</label>
        <input type="checkbox" name="animaux" id="animaux" /> <label for="animaux">Animaux</label>
        <input type="checkbox" name="desing" id="design" /> <label for="design">Design</label>
-   </p>
+      </p>
    </div>
  </form>
 </div>
+
 <style media="screen">
 .upload_img{
-  width: 50%;
-  display: block;
+  width: 70%;
   margin: 0 auto;
   font-family: 'Nunito';
-  font-size: 12px;
+  font-size: 14px;
+  line-height: 30px;
+  font-weight:800;
 }
 .upload_img form{
   width: 100%;
@@ -42,5 +47,20 @@
 .upload_img form input{
   width:70%;
   border-radius: 2px solid black;
+}
+.upload_img form textarea{
+  width:70%;
+}
+.submit{
+  width: 30%;
+}
+.submit input{
+  height: 20px;
+  background-color: black;
+  color: #FFFFFF;
+}
+.description{
+  display: flex;
+  flex-wrap: wrap;
 }
 </style>
