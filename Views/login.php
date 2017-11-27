ffc4a194288a67ceaf88fdc3b0650ae7e1b1f504
Anda <ax.fiolle@gmail.com>
2017-11-27 14:13:48 +0100

    modals register

--- Views/login.php
+++ Views/login.php
@@ -1,9 +1,9 @@
 <form class="formulaire" action="./?action=login" method="post">
-   <label for="username">Veuillez indiquer votre Pseudo ou votre adresse email </label>
-   <input type="text" name="username" id="username" placeholder="Votre pseudo ou email"><br/>
+   <label for="username">Pseudo</label>
+   <input class="fullinput" type="text" name="username" id="username" placeholder="Votre pseudo ou email"><br/>
 
-   <label for="mdp">Veuillez indiquer votre mot de passe</label>
-   <input type="password" name="mdp" id="mdp" placeholder="Votre mot de passe"><br/>
+   <label for="mdp">Mot de passe</label>
+   <input class="fullinput" type="password" name="mdp" id="mdp" placeholder="Votre mot de passe">
 
    <input type="submit" name="button" value="Se Connecter">
 
--- Views/onepicture.php
+++ Views/onepicture.php
@@ -1,7 +1,7 @@
   <div id="onepic" class="modal modalphoto hidden">
     <a id="calltoclose" class="close" href="#">&times;</a>
     <img id="imageview" src="<?= imghold ?>" alt="oneView">
-    <h2 id="imagetitle">Ma photo</h2>
-    <p id="imagedescr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam eaque esse, magnam. Dolore animi, omnis blanditiis recusandae! Reiciendis voluptates, quidem quo. Odio, facere dolorem neque aliquam? Similique nemo, veniam ipsa?</p>
+    <h2 id="imagetitle"></h2>
+    <p id="imagedescr"></p>
   </div>
   
