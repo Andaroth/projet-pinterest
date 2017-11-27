14ded8c5f2626b41d22d9a13320151e5ea62ab0c
Anda <ax.fiolle@gmail.com>
2017-11-27 09:19:12 +0100

    create modals sendPic

--- Views/login.php
+++ Views/login.php
@@ -2,4 +2,9 @@
    <label for="username">Veuillez indiquer votre Pseudo ou votre adresse email </label>
    <input type="text" name="username" id="username" placeholder="Votre pseudo ou email"><br/>
 
+   <label for="mdp">Veuillez indiquer votre mot de passe</label>
+   <input type="password" name="mdp" id="mdp" placeholder="Votre mot de passe"><br/>
+
+   <input type="submit" name="button" value="Se Connecter">
+
 </form>
