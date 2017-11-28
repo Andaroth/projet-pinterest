d74b589d3249eb95f2c303f2c9c244b521c12529
Delphine <delphine@MacBook-Pro-de-Delphine.local>
2017-11-28 12:27:56 +0100

    upload img

--- Views/uploadform.php
+++ Views/uploadform.php
@@ -1,70 +1,142 @@
-<div id="upload_img">
+<div id="container">
+    <a id="calltoclose" class="close" href="#">&times;</a>
+  <div class="titre">
+    <h3>Une nouvelle image?</h3>
+  </div>
+  <div id="upload_img">
     <div class="new_img">
     <form action="./?page=upload" method="post" enctype="multipart/form-data">
-      Une nouvelle image?<br>
-    <div class="img_upload">
-      <label for="file" class="label-file">Choisir une image</label>
-      <input id="file" class="input-file" type="file">
-    </div>
-    <input type="submit" value="Upload Image" name="submit"><br>
-    </div>
-    <div class="description">
-    Titre <br>
-    <input type="text" name="title"><br>
-    Description<br>
-    <textarea name="description" id="description" cols="30" rows="10"></textarea><br>
-      <p>
-       Cochez la/les catégories souhaitées:<br />
-       <input type="checkbox" name="nature" id="nature" /> <label for="nature">Nature</label>
-       <input type="checkbox" name="voyage" id="voyage" /> <label for="voyage">Voyages</label>
-       <input type="checkbox" name="dessin" id="dessin" /> <label for="dessin">Dessin</label>
-       <input type="checkbox" name="animaux" id="animaux" /> <label for="animaux">Animaux</label>
-       <input type="checkbox" name="desing" id="design" /> <label for="design">Design</label>
-      </p>
+      <div class="img_upload">
+        <img id="imgPreview" src="#" alt="">
+        <label for="fileToUpload" class="label-file">Choisir une image</label>
+        <input name="fileToUpload" id="fileToUpload" class="input-file" type="file">
+      </div>
+        <input type="submit" value="Upload Image" name="submit">
+      </div>
+      <div class="description">
+        Titre <br>
+        <input type="text" name="fileTill"><br>
+        Description<br>
+        <textarea name="description" id="description" cols="30" rows="10"></textarea><br>
+      <div class="checkbox">
+         <p>Cochez la/les catégories souhaitées:</p>
+         <div class="item">
+           <input type="checkbox" id="nature"/><label>Nature</label></div>
+         <div class="item1">
+           <input type="checkbox" id="voyage"/><label>Voyages</label>
+         </div>
+         <div class="item2">
+           <input type="checkbox" id="dessin"/><label>Dessins</label>
+         </div>
+         <div class="item3">
+           <input type="checkbox" id="animaux"/><label>Animaux</label>
+         </div>
+         <div class="item4">
+           <input type="checkbox" id="design"/><label>Design</label>
+        </div>
+       </div>
+     </div>
+  </form>
   </div>
- </form>
 </div>
 
 <style media="screen">
 
-#upload_img{
-  width: 40%;
-  font-family: 'Nunito';
-  font-size: 14px;
-  line-height: 20px;
-  font-weight:800;
-  border: 1px solid #ccc;
+#container{
+  width: 60%;
+  border: 1px solid black;
   border-radius: 4px;
+  display: block;
+  margin: 0 auto;
+  opacity: 0.8;
+}
+#container a{
+  display: block;
+  float: right;
+  margin-right: 10px;
+  color: black;
+}
+#upload_img{
+  font-family:'Nunito';
+  font-size: 14px;
+  line-height: 30px;
+  font-weight:800;
   display: flex;
   flex-wrap: wrap;
   margin: 0 auto;
   justify-content: space-between;
+  padding: 20px;
 }
+.titre {
+  width: 60%;
+  text-align: center;
+  display: block;
+  margin: 0 auto;
+  border-bottom: 1px solid black;
+
+}
+.titre h3{
+  font-family: 'Nunito';
+  font-size: 20px;
+}
+/*Upload de l'image*/
 .new_img{
   width: 40%;
 }
 .new_img form{
   padding: 12px;
-  margin-bottom: 10px;
+  margin-bottom: 15px;
   box-sizing: border-box;
   resize: vertical;
 }
-.new_img form input{
-  border-radius: 2px solid black;
-  padding-top: 60%;
+.img_upload label {
+  width: 100%;
+  cursor: pointer;
+  font-weight: bold;
+  text-align: center;
+  display: block;
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%)
 }
-.new_img form textarea{
-
+.img_upload input {
+    display: none;
+}
+input[type="submit"]{
+  display: block;
+  margin: 0 auto;
 }
 .img_upload{
   border-radius: 3px;
   border: 1px solid black;
   width: 200px;
   height: 300px;
-  padding: 20px;
-  margin-bottom: 10px;
+  padding: 10px;
+  margin-bottom: 15px;
 }
+/*Titre, description et catégorie*/
 .description{
-  width: 40%;
+  width: 45%;
+  margin-top: 12px;
+}
+.description input, .description textarea{
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+.description textarea{
+  width: 100%;
+  margin-bottom: 15px;
+}
+.description label{
+  margin-left: 40px;
+  text-align: left;
+}
+.checkbox input, .checkbox label{
+  float:left;
+  clear: both;
+  line-height: normal;
+}
+.item, .item1, .item2, .item3,.item4{
+  display: flex;
 }
 </style>
