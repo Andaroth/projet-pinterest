fce9385c3ea848983a51544317ce0da02f3d0ad7
Delphine <delphine@MacBook-Pro-de-Delphine.local>
2017-11-29 14:23:33 +0100

    fix

--- Models/config.php
+++ Models/config.php
@@ -19,10 +19,6 @@
   ini_set('display_errors', 1);
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
-<<<<<<< HEAD
-   echo 'bonjour';
 
-=======
->>>>>>> axel
   session_start();
 ?>
