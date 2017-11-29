35c78bc018ad460e52a8d03c46009373808725e7
Delphine <delphine@MacBook-Pro-de-Delphine.local>
2017-11-29 14:17:16 +0100

    tablette

--- Views/all.php
+++ Views/all.php
@@ -3,8 +3,10 @@
 <head>
   <meta charset="UTF-8">
   <title><?= $pagetitle ?></title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Nunito%7COpen+Sans" rel="stylesheet">
   <link rel="stylesheet" href="./Views/css/style.css" />
+  <link rel="stylesheet" media="screen and (max-width: 800px)" href="./Views/css/tablette.css">
 </head>
 <body>
   <?php
